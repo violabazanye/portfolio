6e854b25ed300aaa69c1f83f5463887e0793b633
Viola Bazanye <violabazanye@MacBook-Pro.local>
2018-02-28 22:51:20 +0300

    added case studies page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
 import { PrototypesComponent } from './prototypes/prototypes.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
   { path: 'prototypes', component: PrototypesComponent }
 ];
 
