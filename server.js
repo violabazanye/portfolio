674a198773103b38468019b71a737936b9f86a87
Viola Bazanye <violabazanye@MacBook-Pro.local>
2018-03-06 18:12:51 +0300

    deploy to heroku trial 4

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+//add this so the browser can GET the bower files
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080); 
